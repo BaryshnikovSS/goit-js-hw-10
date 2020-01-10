095efdfe020b8a121708027c8c0f36cca264f6b8
S1EpoY <slepoy1992@gmail.com>
2020-01-10 13:59:10 +0200

    fixed_bugs

--- hw-10/src/index.js
+++ hw-10/src/index.js
@@ -1,10 +1,12 @@
-
 import data from './json/menu.json';
 import menuTemplate from './templates/post-menu-items.hbs';
 
 const refs = {
   menuList: document.querySelector('.js-menu'),
+  switchBtn: document.querySelector('.js-switch-input'),
 }
+
+// Создай страницу меню с возможностью выбора темы для сервиса заказа еды.
 
 createMarkup(data, menuTemplate, refs.menuList);
 
@@ -15,23 +17,42 @@ function createMarkup(dataArray, template, refParent) {
   return refParent.insertAdjacentHTML('beforeend', markup);
 }
 
+// Реализуй функционал изменения темы при нажатии (событие change) на чекбокс input.js-switch-input в тулбаре.
+
+// По умолчанию тема светлая.
+// При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme.
+// Выбранная тема должна сохраняться между перезагрузками страницы. 
+// Для хранения активной темы используй localStorage.
+// Если при загрузке страницы тема темная, не забудь поставить свойство checked 
+// у чекбокса input.js-switch-input в true, чтобы ползунок сдвинулся в правильное положение.
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-document.body.classList.add(Theme.LIGHT);
+const defaultTheme = selectedTheme();
+document.body.classList.add(defaultTheme);
 
-const changeTheme = function() {
-
-  if (input.checked) {
-    document.body.classList.replace(Theme.LIGHT, Theme.DARK)
+function selectedTheme() {
+  if (localStorage.getItem('Theme') === null) {
+    return Theme.LIGHT;
   } else {
-    document.body.classList.replace(Theme.DARK, Theme.LIGHT)
-  };
-
+    if (localStorage.getItem('Theme') === Theme.DARK) {
+      refs.switchBtn.setAttribute('checked', true);
+    }
+    return localStorage.getItem('Theme');
+  }
 }
 
-const input = document.querySelector('.js-switch-input');
+refs.switchBtn.addEventListener('change', changeTheme);
 
-input.addEventListener('change', changeTheme);
+function changeTheme(e) {
+  if (e.currentTarget.checked) {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.LIGHT);
+  };
+}
